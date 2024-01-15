84de480619910f4909c93da5e7d95cfa6ba9fdd2
Mugdhashrivastava <shrivastavamugdha19@gmail.com>
2024-01-15 15:00:37 +0530

    work in progress

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,22 +1,72 @@
+import { useState ,useEffect  } from "react";
+import styled from "styled-components";
+import SearchResult from "./Component/SearchResults";
+
+// npm run server port (node backend)
+const BASE_URL = "http://localhost:9000/";
+
+const App = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, seterror] = useState(null);
+
+  
 
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-  <div>sdfvsdfva</div>
-  )
-}
-
-export default App;
 
 
+  useEffect(() => {
+      const fetchFoodData = async () => {
+        setLoading(true);
+        try {
+          const response = await fetch(BASE_URL);
+          const json = await response.json();
+          setData(true);
+          // console.log(json);
+          setLoading(false);
+        } catch (error) {
+          seterror("Unable to fetch data")
+        }
+      };
+      fetchFoodData();
+    },[]);
 
-export const Container = styled.div`
+    console.log(data);
+
+    if (error) return <div>{error}</div>
+    if (loading) return <div>loading......</div>
+
+    return (
+      <Container>
+        <TopContainer>
+          <div className="logo">
+            <img src="/logo.svg" alt="logo" />
+          </div>
+
+          <div className="search">
+            <input placeholder="Search Food...." />
+          </div>
+        </TopContainer>
+
+        <FilterContainer>
+          <Button>All</Button>
+          <Button>Breakfast</Button>
+          <Button>Lunch</Button>
+          <Button>Dinner</Button>
+        </FilterContainer>
+
+        <SearchResult data ={data} />
+      </Container>
+    );
+  };
+
+  export default App;
+
+  export const Container = styled.div`
   max-width: 1200px;
   margin: 0 auto;
 `;
-const TopContainer = styled.section`
+  const TopContainer = styled.section`
   height: 140px;
   display: flex;
   justify-content: space-between;
@@ -44,14 +94,14 @@ const TopContainer = styled.section`
   }
 `;
 
-const FilterContainer = styled.section`
+  const FilterContainer = styled.section`
   display: flex;
   justify-content: center;
   gap: 12px;
   padding-bottom: 40px;
 `;
 
-export const Button = styled.button`
+  export const Button = styled.button`
   background: ${({ isSelected }) => (isSelected ? "#f22f2f" : "#ff4343")};
   outline: 1px solid ${({ isSelected }) => (isSelected ? "white" : "#ff4343")};
   border-radius: 5px;
@@ -63,7 +113,3 @@ export const Button = styled.button`
     background-color: #f22f2f;
   }
 `;
-
-
-
-
